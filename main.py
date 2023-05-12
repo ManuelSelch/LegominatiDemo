56c28f1910127412843c5cc233e043fed9975453
manuelselch <manuel@selch.de>
2023-05-12 14:22:02 +0200

    S1 color sensor

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ left_motor = Motor(Port.B)
 right_motor = Motor(Port.C)
 
 # Initialize the color sensor.
-Farbsensor = ColorSensor(Port.S3)
+Farbsensor = ColorSensor(Port.S1)
 
 # Initialize the drive base.
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
@@ -81,14 +81,4 @@ while True:
     if Button.LEFT in ev3.buttons.pressed():
         run4()
 
-    # Calculate the deviation from the threshold.
-    deviation = line_sensor.reflection() - threshold
-
-    # Calculate the turn rate.
-    turn_rate = PROPORTIONAL_GAIN * deviation
-
-    # Set the drive base speed and turn rate.
-    robot.drive(DRIVE_SPEED, turn_rate)
-
-    # You can wait for a short time or do other things in this loop.
-    wait(10)
+    
